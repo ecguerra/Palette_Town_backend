6282f3a1c95b9d182be4a9f3a4387d184858cfaa
Elizabeth Guerra <ecguerra@outlook.com>
2021-02-16 21:44:49 -0500

    added Del routes to ColorPalette and SavedPalette

--- resources/color_palettes.py
+++ resources/color_palettes.py
@@ -12,4 +12,8 @@ def create_color_palette():
     color_palette_dict = model_to_dict(color_palette)
     return jsonify(data=color_palette_dict, status={"code": 201, "message": "Successfully created"})
 
-# Need a Delete route to remove a color from a palette, but not delete either of those
+@color_palettes.route('/<id>', methods=['Delete'])
+def delete_color_palette(id):
+    cp_to_delete = models.ColorPalette.get_by_id(id)
+    cp_to_delete.delete_instance()
+    return jsonify(data={},status={"code": 200, "message": "Successfully deleted"})
--- resources/saved_palettes.py
+++ resources/saved_palettes.py
@@ -13,4 +13,8 @@ def create_saved_palette():
     saved_palette_dict = model_to_dict(saved_palette)
     return jsonify(data=saved_palette_dict, status={"code": 201, "message": "Successfully created"})
 
-# need a Delete route to "unsave" - don't delete user or palette
+@saved_palettes.route('/<id>', methods=['Delete'])
+def delete_saved_palette(id):
+    sp_to_delete = models.SavedPalette.get_by_id(id)
+    sp_to_delete.delete_instance()
+    return jsonify(data={},status={"code": 200, "message": "Successfully deleted"})
