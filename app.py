e31bae01af1ef67f66e2c9747fc6d9107fe667a1
Elizabeth Guerra <ecguerra@outlook.com>
2021-02-22 21:40:43 -0500

    more debugging

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ PORT=8000
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
 
 CORS(app, \
      origins=['http://localhost:3000','https://palettetown.netlify.app'], \
